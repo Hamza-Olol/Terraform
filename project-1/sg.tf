220fd2035a3477a04598b41e6e1b6b6e7d3983ce
Hamza-Olol <hamza.olol@slalom.com>
2022-09-27 16:18:57 +0100

    Clean up + adding project-1

---
+++ project-1/sg.tf
@@ -0,0 +1,60 @@
+# ssh security group
+resource "aws_security_group" "sg-ssh" {
+  name        = "sgssh"
+  description = "Allow SSH into an instance"
+
+  ingress {
+    description = "SSH from port 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    # Fill in below with your own ip to be more secure and not allow access to everyone
+    cidr_blocks = [var.sg_ingress_ip]
+  }
+
+  egress {
+    description = "Allow all ip and ports outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg-ssh-tf-HO"
+  }
+}
+
+# Web security group 
+
+resource "aws_security_group" "sg-web" {
+  name        = "sgweb"
+  description = "Allow web access"
+  ingress {
+    description = "Allow Port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    # Fill in below with your own ip to be more secure and not allow access to everyone
+    cidr_blocks = [var.sg_ingress_ip]
+  }
+  ingress {
+    description = "Allow Port 443"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    # Fill in below with your own ip to be more secure and not allow access to everyone
+    cidr_blocks = [var.sg_ingress_ip]
+  }
+  egress {
+    description = "Allow all ip and ports outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg-web-tf-HO"
+  }
+}
